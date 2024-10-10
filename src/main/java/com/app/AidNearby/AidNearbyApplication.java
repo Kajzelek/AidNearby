cdbd6a708f1e332ac5963ab3d2fff0497701bb29
Kajzelek <cygnar.patryk@gmail.com>
2024-10-10 12:23:19 +0200

    project initilization

---
+++ src/main/java/com/app/AidNearby/AidNearbyApplication.java
@@ -0,0 +1,13 @@
+package com.app.AidNearby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AidNearbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AidNearbyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/AidNearby/AidNearbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.AidNearby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AidNearbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
